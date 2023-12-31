08054cd2541cf53829c569e1df001f012fd039f4
Mukesh Maurya <mukesh.ice17@gmail.com>
2023-12-31 12:28:07 +0530

    alert service added to show alerts app wide

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,21 @@
 import React from "react";
-
+import { TMAlert } from "../../services/alertService";
 export const Home = (props) => {
+  const openAlert = () => {
+    TMAlert.showAlert("danger", "I am danger alert!!!!");
+  };
   return (
     <>
       <h3 className="mike">Home Component</h3>
+      <div id="liveAlertPlaceholder"></div>
+      <button
+        type="button"
+        className="btn btn-primary"
+        id="liveAlertBtn"
+        onClick={() => openAlert()}
+      >
+        Show live alert
+      </button>
     </>
   );
 };
