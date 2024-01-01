10aa27b5f34d785bf3e6b15762586da1ad21faf9
Mukesh Maurya <mukesh.ice17@gmail.com>
2024-01-01 17:35:09 +0530

    routing issue fixed

--- src/components/pages/Backlogs.js
+++ src/components/pages/Backlogs.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 export const Backlogs = ({ blTasks }) => {
   const navigate = useNavigate();
   const goToDetail = (id) => {
-    navigate("detail/" + id);
+    navigate("/detail/" + id);
   };
   return (
     <>
