9e6572acdba4c241ff62e8acc3effcdd40e20031
Mukesh Maurya <mukesh.ice17@gmail.com>
2024-02-11 19:19:37 +0530

    tweak public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "/build"),
     filename: "tm/bundle.js",
-    publicPath: "/",
+    publicPath: "/tm",
   },
   mode: "production",
   plugins: [
