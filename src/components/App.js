bcd8004ea87512819aa17816f282f53e133faffa
Mukesh Maurya <mukesh.ice17@gmail.com>
2024-02-11 10:30:37 +0530

    mobile device UI warnig

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ import { Backlogs } from "./pages/Backlogs";
 import { Detail } from "./pages/Detail";
 import { Archived } from "./pages/Archived";
 import ErrorBoundary from "../containers/ErrorBoundry";
+import { TMAlert } from "../services/alertService";
 
 class App extends Component {
   options = ["One", "Two", "Three", "Four", "Five"];
@@ -21,6 +22,21 @@ class App extends Component {
     };
   }
   componentDidMount() {
+    ///detect device type
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      // Take the user to a different screen here.
+      appObj.isMobile = true;
+      TMAlert.showAlert(
+        "danger",
+        "This is desktop only app, UI may look wiered on mobile!!!!!"
+      );
+    } else {
+      appObj.isMobile = false;
+    }
     //Enabling bs tooltip
     var tooltipTriggerList = [].slice.call(
       document.querySelectorAll('[data-bs-toggle="tooltip"]')
